d8414cd99fe42c94d2e8059538c75f0b9710f7f7
Victor Bergene <v@walmin.com>
2021-08-29 20:26:31 +0200

    Fetching json data & showing in a list

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View, ActivityIndicator, FlatList } from 'react-native';
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+     try {
+      const response = await fetch('https://polisen.se/api/events?locationname=Uppsala;Härnösand');
+      const json = await response.json();
+      setData(json);
+
+    } catch (error) {
+      console.error(error);
+
+    } finally {
+      setLoading(false);
+
+    }
+
+  };
+
+  useEffect(() => { getData(); }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Crossplatformutveckling App</Text>
-      <StatusBar style="auto" />
+      {isLoading ? <ActivityIndicator/> : (
+        <View>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <View style={styles.item}>
+                <Text style={styles.title}>{item.name}</Text>
+                <Text style={styles.text}>{item.summary}</Text>
+              </View>
+            )}
+          />
+        </View>
+        )}
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -17,5 +52,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: '2vmin'
   },
+  item: {
+    padding: '1vmin',
+    marginTop: "0.5vmin",
+    flexDirection: "row",
+    textAlign: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#f2f2f2",
+    borderRadius: "0.5vmin"
+  },
+  title: {
+    fontWeight: 'bold',
+    padding: 0,
+    margin: 0
+  },
+  text: {
+    fontWeight: 'normal',
+    padding: 0,
+    margin: 0
+  }
 });
